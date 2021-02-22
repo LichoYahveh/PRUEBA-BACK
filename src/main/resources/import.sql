2c6e71f7abd13f1e419ba9a5f0449ec415b4748d
LichoYahveh <39471556+LichoYahveh@users.noreply.github.com>
2021-02-21 20:25:49 -0500

    Se añade clientes de prueba

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `prueba`.`clientes`(`apellido`,`nombre`)VALUES('MERA MENDOZA','DOUGLAS LIZANDRO');
+INSERT INTO `prueba`.`clientes`(`apellido`,`nombre`)VALUES('VEGA VEGA','RICARDO GIAN');
+INSERT INTO `prueba`.`clientes`(`apellido`,`nombre`)VALUES('BARCO VEGA','PABLO ANDRES');
